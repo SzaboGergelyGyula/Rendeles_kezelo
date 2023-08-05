3d17ce149f7ec27c8a3bd0d6b838e6790cc09527
Gergely Gyula Szabó <szabo.gergely.gyula@gmail.com>
2023-08-05 14:56:38 +0200

    base and preload example, with type errors

---
+++ src/preload/index.ts
@@ -0,0 +1,29 @@
+import { contextBridge, ipcRenderer } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
+
+// Custom APIs for renderer
+const api = {
+  getProfileInfo: async (args) => {
+    const result = await ipcRenderer.invoke('get-user', args)
+    return result
+  }
+  // node: () => process.versions.node
+  // ping: () => ipcRenderer.invoke('ping')
+}
+
+// Use `contextBridge` APIs to expose Electron APIs to
+// renderer only if context isolation is enabled, otherwise
+// just add to the DOM global.
+if (process.contextIsolated) {
+  try {
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', api)
+  } catch (error) {
+    console.error(error)
+  }
+} else {
+  // @ts-ignore (define in dts)
+  window.electron = electronAPI
+  // @ts-ignore (define in dts)
+  window.api = api
+}
